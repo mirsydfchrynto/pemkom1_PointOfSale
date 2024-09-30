98d0338d5ba484d24c0aef5c784da56e249885e7
mirsydfchrynto <mirsyadf1805@gmail.com>
2024-09-30 20:59:51 +0700

    pertemuan2

---
+++ pertemuan2/loginn.java
@@ -0,0 +1,19 @@
+
+package pertemuan2;
+
+/**
+ *
+ * @author HP
+ */
+public class loginn {
+    
+
+    public static void main (String[] args) {
+        
+        Register rg = new Register();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(Register.EXIT_ON_CLOSE);
+    }
+}
